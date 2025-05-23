06d1458e235d31cfd9e83511d53044e80668f5f0
avilamrales <jesusavilamrales@protonmail.com>
2025-05-23 17:37:59 -0600

    Added swagger docs to the backend

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -2,11 +2,21 @@ import { NestFactory } from '@nestjs/core';
 import { ValidationPipe } from '@nestjs/common';
 
 import { AppModule } from './app.module';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   app.useGlobalPipes(new ValidationPipe({ whitelist: true }));
+
+  const config = new DocumentBuilder()
+    .setTitle('NestJS API')
+    .setDescription('NestJS API description')
+    .setVersion('1.0')
+    .addTag('products')
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api/docs', app, document);
 
   app.setGlobalPrefix('api');
 
--- backend/src/products/products.controller.ts
+++ backend/src/products/products.controller.ts
@@ -10,31 +10,68 @@ import {
 import { ProductsService } from './products.service';
 import { CreateProductDto } from './dto/create-product.dto';
 import { UpdateProductDto } from './dto/update-product.dto';
+import { ApiOperation, ApiResponse, ApiTags } from '@nestjs/swagger';
 
+@ApiTags('products')
 @Controller('products')
 export class ProductsController {
   constructor(private readonly productsService: ProductsService) {}
 
+  @ApiOperation({
+    summary: 'Create a new product',
+  })
+  @ApiResponse({
+    status: 201,
+    description: 'Product created successfully',
+  })
   @Post()
   create(@Body() createProductDto: CreateProductDto) {
     return this.productsService.create(createProductDto);
   }
 
+  @ApiOperation({
+    summary: 'Get all products',
+  })
+  @ApiResponse({
+    status: 200,
+    description: 'Return all products',
+  })
   @Get()
   findAll() {
     return this.productsService.findAll();
   }
 
+  @ApiOperation({
+    summary: 'Get a product by ID',
+  })
+  @ApiResponse({
+    status: 200,
+    description: 'Return a product',
+  })
   @Get(':id')
   findOne(@Param('id') id: string) {
     return this.productsService.findOne(id);
   }
 
+  @ApiOperation({
+    summary: 'Update a product by ID',
+  })
+  @ApiResponse({
+    status: 200,
+    description: 'Product updated successfully',
+  })
   @Patch(':id')
   update(@Param('id') id: string, @Body() updateProductDto: UpdateProductDto) {
     return this.productsService.update(id, updateProductDto);
   }
 
+  @ApiOperation({
+    summary: 'Delete a product by ID',
+  })
+  @ApiResponse({
+    status: 200,
+    description: 'Product deleted successfully',
+  })
   @Delete(':id')
   remove(@Param('id') id: string) {
     return this.productsService.remove(id);
